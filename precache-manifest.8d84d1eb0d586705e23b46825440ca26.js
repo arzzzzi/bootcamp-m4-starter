667fcaff213bda80ab29d853d4fdab3095695840
arzzzzi <kantyshevmagomed@gmail.com>
2022-01-31 12:42:50 +0300

    init

---
+++ precache-manifest.8d84d1eb0d586705e23b46825440ca26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508abb7f8c9e4cc51123bf600b0a483c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec3731f108ac06f12273",
+    "url": "/static/css/main.0a3eead4.chunk.css"
+  },
+  {
+    "revision": "67b984181bc60c03486a",
+    "url": "/static/js/2.6a24eefa.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/static/js/2.6a24eefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3731f108ac06f12273",
+    "url": "/static/js/main.3c7d9c29.chunk.js"
+  },
+  {
+    "revision": "97d614f48baeada74120",
+    "url": "/static/js/runtime-main.47eaace9.js"
+  }
+]);
